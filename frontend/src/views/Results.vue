54b4b48d7841a31ced453112a5e0971e032b38d1
Mike Sova <michalpso@gmail.com>
2019-04-12 01:59:57 -0500

    date formatting

--- frontend/src/views/Results.vue
+++ frontend/src/views/Results.vue
@@ -8,10 +8,26 @@
     </h1>
     <v-container>
       <GChart
+        type="ColumnChart"
+        :data="aggChartData"
+        :options="aggChartOptions"
+      />
+      <GChart
         type="LineChart"
         :data="chartData"
         :options="chartOptions"
       />
+      <GChart
+        type="ColumnChart"
+        :data="aggChartDataHour"
+        :options="aggChartOptionsHour"
+      />
+      <GChart
+        type="LineChart"
+        :data="chartDataHour"
+        :options="chartOptionsHour"
+      />
+
     <!-- <v-data-table
       :headers="headers"
       :items="data"
@@ -58,19 +74,68 @@
     computed: {
       chartData(){
         const agg_day = JSON.parse(this.data.grouped_day)
-        console.log(agg_day)
-        const result = [['Number of','Assault','Battery','Burglary']]
+        const result = [['Date','Assault','Battery','Burglary',"Robbery", "Sex offense", "Stalking", "Weapons violation",]]
         const dates = this.next7Days()
         agg_day.forEach((value, index) => {
-          result.push([dates[index], value['Assault'],value['Battery'],value['Burglary']])
+          result.push([dates[index], value['Assault'],value['Battery'],value['Burglary'], value["Robbery"], value["Sex offense"], value["Stalking"], value["Weapons violation"],
+          ])
         })
-        console.log(result)
         return result
       },
       chartOptions() {
         return { 
-         title: 'Number of predicted crimes per day for '+ this.district.name, subtitle: 'Number of crimes each day' ,
-         height: 330
+         title: 'Number of predicted crimes per type for each day in '+ this.district.name + ' district', subtitle: 'Number of crimes each day' ,
+         height: 430
+        }
+      },
+      aggChartData(){
+        const agg_day = JSON.parse(this.data.grouped_day)
+        const result = [['Date','total']]
+        const dates = this.next7Days()
+        agg_day.forEach((value, index) => {
+          const sum = value['Assault']+ value['Battery']+ value['Burglary']+  value["Robbery"]+  value["Sex offense"]+  value["Stalking"]+  value["Weapons violation"]
+          result.push([dates[index], sum])
+        })
+        return result
+      },
+      aggChartOptions() {
+        return { 
+         title: 'Number of all predicted crimes per day in '+ this.district.name + ' district', subtitle: 'Total Number of crimes each day' ,
+         height: 430
+        }
+      },
+      aggChartDataHour(){
+        let agg_day = JSON.parse(this.data.grouped_hour)
+        agg_day = agg_day.slice(0,25)
+        const result = [['Date','total']]
+        const dates = this.next24hours()
+        agg_day.forEach((value, index) => {
+          const sum = value['Assault']+ value['Battery']+ value['Burglary']+  value["Robbery"]+  value["Sex offense"]+  value["Stalking"]+  value["Weapons violation"]
+          result.push([dates[index], sum])
+        })
+        return result
+      },
+      aggChartOptionsHour() {
+        return { 
+         title: 'Number of all predicted crimes per hour in '+ this.district.name + ' district', subtitle: 'Total Number of crimes each hour' ,
+         height: 430
+        }
+      },
+      chartDataHour(){
+        let agg_day = JSON.parse(this.data.grouped_hour)
+        agg_day = agg_day.slice(0,25)
+        const result = [['Date','Assault','Battery','Burglary',"Robbery", "Sex offense", "Stalking", "Weapons violation",]]
+        const dates = this.next24hours()
+        agg_day.forEach((value, index) => {
+          result.push([dates[index], value['Assault'],value['Battery'],value['Burglary'], value["Robbery"], value["Sex offense"], value["Stalking"], value["Weapons violation"],
+          ])
+        })
+        return result
+      },
+      chartOptionsHour() {
+        return { 
+         title: 'Number of predicted crimes per type for each hour in '+ this.district.name + ' district', subtitle: 'Number of crimes each hour' ,
+         height: 430
         }
       },
     },
@@ -85,9 +150,25 @@
         date = `${mm}/${dd}/${yy}`;
         return date
       },
+      formatHourDate(d) {
+        var amOrPm = (d.getHours() < 12) ? "AM" : "PM";
+        var hour = (d.getHours() < 12) ? d.getHours() : d.getHours() - 12;
+        return   d.getDate() + ' / ' + d.getMonth() + ' / ' + ' ' + hour + amOrPm;
+      },
+      next24hours() {
+        const result = []
+        for (let i = 1  ; i <= 25; i++) {
+          const hours = this.time.split(':')[0]
+          const d = new Date(this.date);
+          d.setHours(d.getHours() + hours);
+          d.setHours(d.getHours() + i);
+          result.push(this.formatHourDate(d))
+        }
+        return result
+      },
       next7Days() {
         const result = []
-        for (let i = 1; i < 8; i++) {
+        for (let i = 1  ; i < 9; i++) {
           const d = new Date(this.date);
           d.setDate(d.getDate() + i);
           result.push(this.formatDate(d))
