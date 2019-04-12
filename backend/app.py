bf693d9e0ca518c000129d0641e83f0ea367003a
Mike Sova <michalpso@gmail.com>
2019-04-12 01:26:08 -0500

    fixed some stuff

--- backend/app.py
+++ backend/app.py
@@ -18,7 +18,7 @@ CORS(app)
 
 from datetime import datetime, timedelta
 
-def get_times(dt):
+def get_date_features(dt):
     weekday = dt.weekday()
     dayofyear = dt.timetuple().tm_yday
     week = dt.isocalendar()[1]
@@ -32,29 +32,39 @@ def get_times(dt):
 
 def get_next_week(dt):
     delta = timedelta(hours=1)   
-    time_range = [get_times(dt)]
+    date_features = [get_date_features(dt)]
+    time_range = [dt]
     for _ in range(168):
         dt += delta
-        time_range.append(get_times(dt))
-    return time_range
+        date_features.append(get_date_features(dt))
+        time_range.append(dt)
+    return date_features,time_range
 
 def load_model(number):
     filename = "district_"+number+".pkl"
     model = load("models/"+filename) 
     return model
 
-def create_df(rows):
+def create_df(rows, time_range):
     columns = ['Assault', 'Battery', 'Burglary', 'Concealed carry license violation', 'Crime sexual assault', 'Criminal damage', 'Criminal trespass', 'Deceptive practice', 'Gambling', 'Homicide', 'Human trafficking', 'Interference with public officer', 'Intimidation', 'Kidnapping', 'Liquor law violation', 'Motor vehicle theft', 'Narcotics', 'Non-criminal', 'Non-criminal (subject specified)', 'Obscenity', 'Offense involving children', 'Other narcotic violation', 'Other offense', 'Prostitution', 'Public indecency', 'Public peace violation', 'Robbery', 'Sex offense', 'Stalking', 'Theft', 'Weapons violation']
-    df = pd.DataFrame(rows, columns = columns)
+    df = pd.DataFrame(rows, columns = columns, index=time_range)
     return df 
 
 def get_model_prediction_for_district(number,dt):
     model = load_model(number)
-    time_ranges = get_next_week(dt)
-    predictions = model.predict(time_ranges)    
-    df = create_df(predictions)
+    data_features,time_range = get_next_week(dt)
+    predictions = model.predict(data_features)    
+    df = create_df(predictions, time_range)
+    # group by hour and day
+    grouped_hour = df.resample('H').sum()
+    agg_grouped_hour = grouped_hour.sum(axis=0).to_json(orient='records')
+    grouped_hour = grouped_hour.to_json(orient='records')
+    # grouped_hour = 
+    grouped_day = df.resample('D').sum()
+    agg_grouped_day = grouped_day.sum(axis=0).to_json(orient='records')
+    grouped_day = grouped_day.to_json(orient='records')
     records = df.to_json(orient='records')
-    return records
+    return (grouped_hour, agg_grouped_hour, grouped_day, agg_grouped_day, records)
 
 # sanity check route
 @app.route('/ping', methods=['GET'])
@@ -68,8 +78,11 @@ def models():
     district = request.args.get('district')
     datestring = date + ' ' + time
     dt = datetime.strptime(datestring, '%Y-%m-%d %H:%M')
-    resp = get_model_prediction_for_district(district, dt)
-    return jsonify(resp)
+    grouped_hour, agg_grouped_hour, grouped_day, agg_grouped_day, records = get_model_prediction_for_district(district, dt)
+    response = {
+        "grouped_hour":grouped_hour, "agg_grouped_hour":agg_grouped_hour, "grouped_day":grouped_day, "agg_grouped_day":agg_grouped_day, "records":records,
+    }
+    return jsonify(response)
 
 
 if __name__ == '__main__':
--- frontend/src/views/Results.vue
+++ frontend/src/views/Results.vue
@@ -2,12 +2,17 @@
   <div>
     <h1>
       <knifeline/>
-        <h4>Predicted crimes for {{district.number}} {{district.name}} District </h4>
-        <h4>on date: {{date}} at time: {{time}}</h4>
+        <h4>Predicted crimes for {{district.name}} ({{district.number}} district) </h4>
+        <h4>starting date: {{date}} and starting time: {{time}}</h4>
       <knifeline/>     
     </h1>
     <v-container>
-    <v-data-table
+      <GChart
+        type="LineChart"
+        :data="chartData"
+        :options="chartOptions"
+      />
+    <!-- <v-data-table
       :headers="headers"
       :items="data"
       class="elevation-1"
@@ -16,7 +21,7 @@
         <td>{{ props.item.name }}</td>
         <td>{{ props.item.number }}</td>
       </template>
-    </v-data-table>
+    </v-data-table> -->
     </v-container>
     <router-link to="/">Home</router-link>
   </div>
@@ -24,9 +29,12 @@
 
 <script>
   import knifeline from '../components/knifeline.vue' 
+  import { GChart } from 'vue-google-charts'
+
   export default {
     name: "Results",
-    components: {knifeline},
+    components: {knifeline,GChart
+},
     props: ['time','date','district','data'],
     data () {
       return {
@@ -45,6 +53,46 @@
           },
 
         ],
+      }
+    },
+    computed: {
+      chartData(){
+        const agg_day = JSON.parse(this.data.grouped_day)
+        console.log(agg_day)
+        const result = [['Number of','Assault','Battery','Burglary']]
+        const dates = this.next7Days()
+        agg_day.forEach((value, index) => {
+          result.push([dates[index], value['Assault'],value['Battery'],value['Burglary']])
+        })
+        console.log(result)
+        return result
+      },
+      chartOptions() {
+        return { 
+         title: 'Number of predicted crimes per day for '+ this.district.name, subtitle: 'Number of crimes each day' ,
+         height: 330
+        }
+      },
+    },
+    methods:{
+      formatDate(date) {
+        let dd = date.getDate();
+        let mm = date.getMonth() + 1;
+        const yy = date.getFullYear().toString().substr(-2);
+        // pull the last two digits of the year
+        if (dd < 10) { dd = `0${dd}` }
+        if (mm < 10) { mm = `0${mm}` }
+        date = `${mm}/${dd}/${yy}`;
+        return date
+      },
+      next7Days() {
+        const result = []
+        for (let i = 1; i < 8; i++) {
+          const d = new Date(this.date);
+          d.setDate(d.getDate() + i);
+          result.push(this.formatDate(d))
+        }
+        return result
       }
     }
   }
